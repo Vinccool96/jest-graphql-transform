32a82dabcaff9ad95e450173a4b23843477f40f1
Vincent Girard <vinccool96@gmail.com>
2022-11-29 12:47:14 -0500

    Completed

--- jest.config.ts
+++ jest.config.ts
@@ -3,16 +3,19 @@ import type { Config } from "@jest/types"
 const config: Config.InitialOptions = {
   preset: "ts-jest",
   testEnvironment: "node",
-  moduleFileExtensions: ["js", "json", "ts"],
+  clearMocks: true,
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/$1",
     "^~/(.*)$": "<rootDir>/$1",
   },
+  extensionsToTreatAsEsm: [".ts"],
+  moduleFileExtensions: ["js", "ts", "json"],
   transform: {
-    "\\.(gql|graphql)$": "./dist/index.js",
-    "\\.ts$": "ts-jest",
-    ".*": "babel-jest",
+    "\\.(j|t)s$": "ts-jest",
+    "\\.(gql|graphql)$": "<rootDir>/dist/index.js",
   },
+  moduleDirectories: ["node_modules"],
+  verbose: true,
 }
 
 export default config
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,55 @@
+import { resolve } from "path"
+import { readFileSync } from "fs"
+
 import loader from "graphql-tag"
-import {SyncTransformer, TransformOptions} from "@jest/transform"
+import { SyncTransformer } from "@jest/transform"
 
+function indexesOf<E>(
+  array: Array<E>,
+  predicate: (value: E, index: number, array: Array<E>) => boolean
+): Array<number> {
+  const indexes: Array<number> = []
+  for (let i = 0; i < array.length; i++) {
+    const arrayElement = array[i]
+    if (predicate(arrayElement, i, array)) {
+      indexes.push(i)
+    }
+  }
+  return indexes
+}
 
+function doImports(
+  sourceLines: Array<string | Array<string>>,
+  sourcePath: string,
+  loadedFiles: Set<string> = new Set()
+): Array<string> {
+  if (loadedFiles.has(sourcePath)) {
+    return []
+  }
+
+  loadedFiles.add(sourcePath)
+
+  const indexesImportLines = indexesOf(sourceLines, (s) => /^# ?import "[^"]+"$/g.test(s as string))
+
+  for (const index of indexesImportLines) {
+    const relativeImportPath = (sourceLines[index] as string).split('"')[1]
+    const importedFilePath = resolve(sourcePath, "..", relativeImportPath)
+    const importedFileLines = readFileSync(importedFilePath, { encoding: "utf8" }).split("\n")
+    sourceLines[index] = doImports(importedFileLines, importedFilePath, loadedFiles)
+  }
+
+  return sourceLines.flat()
+}
 
 const graphqlTransformer: SyncTransformer = {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
-  process(sourceText: string, sourcePath: string, options: TransformOptions) {
-    const commentLines = sourceText.split(/\n/)
-    const importLines = commentLines.filter(s => /^# ?import "[^"]+"$/g.test(s))
+  process(sourceText: string, sourcePath: string) {
+    const sourceLines = sourceText.split(/\n/)
+    const linesWithImport = doImports(sourceLines, sourcePath)
+    const foldedLines = linesWithImport.reduce((prev, curr) => `${prev}\n${curr}`)
 
-    const result = loader.call({ cacheable() {} }, sourceText)
-    return loader.call({ cacheable() {} }, sourceText)
+    return loader.call({ cacheable() {} }, foldedLines)
   },
 }
 
