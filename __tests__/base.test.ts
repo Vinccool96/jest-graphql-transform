c1b4177da756132d63e5d352c0cb09961ddc604c
Vincent Girard <vinccool96@gmail.com>
2022-11-29 12:49:29 -0500

    fix

--- __tests__/base.test.ts
+++ __tests__/base.test.ts
@@ -1,9 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import fooQ from "@/graphql/query/fooQuery.gql"
 
-describe('a', function () {
-  it('should b', function () {
+describe("a", function () {
+  it("should b", function () {
     const fQ = fooQ
     expect(1).toEqual(1)
-  });
-});
+  })
+})
